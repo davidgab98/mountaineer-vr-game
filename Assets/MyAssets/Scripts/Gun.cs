d7698dbf6b2e3ab5a48f676633cad972dfe0ed3c
david98ab <davidgab08@gmail.com>
2020-11-17 00:01:57 +0100

    Feature: Adding Doors, Levers, Drawers and more interactable objects with joints

--- Assets/MyAssets/Scripts/Gun.cs
+++ Assets/MyAssets/Scripts/Gun.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class Gun : MonoBehaviour
 {
+    public GameObject bullet;
+    float bulletSpeed = 200f;
+
     public AudioSource audioSource;
     public AudioClip audioClip;
 
     // En el tuturial de Valem hay mas cosas ricas en este script, como lanzar una bala
     public void Fire() {
+        GameObject spawnedBullet = Instantiate(bullet, transform.position, Quaternion.identity);
+        spawnedBullet.GetComponent<Rigidbody>().AddForce(transform.forward * bulletSpeed);
+
         audioSource.PlayOneShot(audioSource.clip);
     }
 }
